1de3ec4e045bc417ffab58f8baca24cedf9c57e8
Lae <lae@utmuser48-187.wireless.utoronto.ca>
2013-11-12 14:23:05 -0500

    start over with API

--- client.py
+++ client.py
@@ -1,31 +1,31 @@
-from socket import *
-try:
-    import RPi.GPIO as GPIO # general purpose input-output for the RP
-except RuntimeError:
-    print("Error importing RPi.GPIO!  This is probably because you need superuser privileges.  You can achieve this by using 'sudo' to run your script")
+#! /usr/bin/env python2.7
+import socket 
 
-HOST = #master
-PORT = 5001
-s = socket(AF_INET, SOCK_STREAM)
-s.connect((HOST, PORT))
+HOST = '127.0.0.1'    # The remote host
+PORT = 50008             # The same port as used by the server
 
-GPIO.setmode(GPIO.BOARD) # I think it is better to use BOARD
-channel = #channel number based on the numbering system have specified (BOARD or BCM)
 
-GPIO.setup(channel, GPIO.IN) #set up incoming channel for the pion
+def establish_connection():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((HOST, PORT))
+    return s
 
-pion_input = False # initialize just in case, not sure how it works for now, may have defaulted to false right away
-pion_input = GPIO.input(channel)
+def perform_tasks():
+    s = establish_connection()
+    while 1:
+        data = s.recv(1024)
+        print 'Received', repr(data)
+        s.send('Hello, world')
 
-while 1:
-    if (pion_input): # things happened!
-        # get things unpacked from the input
-        
-        # TODO: limit the running time? we have to protect from a stupid output.
-        # TODO: in case of any error, we should send the signal back indicating that pion couldn't make it 
-        # do the work
-        
-        
-        s.send(answer, id) # send result of work back to server
-        # s.close()
-        # print 'Received', repr(data)
+        if data == "end":
+            break
+    s.close()
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    perform_tasks()
