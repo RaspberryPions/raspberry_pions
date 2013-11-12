a2ba815cdc01fc803c996adfaa62bc1081754ca3
Yuliya Cherenkova <layshu@gmail.com>
2013-11-12 15:00:03 -0500

    proof of concept

--- server.py
+++ server.py
@@ -3,20 +3,62 @@
 import socket
 
 BLOCK_SIZE = 1024
-PORT = 50008
+PORT = 50007
 HOST = '127.0.0.1'
 
-s = socket.socket()
-s.bind((HOST, PORT))
 
-s.listen(0)
-c, addr = s.accept()
-print("Connection from", addr)
+NUM_PEONS = 2
 
-while 1:
 
-    c.send("blah")
-    peon_message = c.recv(BLOCK_SIZE)
-    print(peon_message)
+# in the beginning:
+    # get available peons from overlay
 
-c.close()
+
+class Task(object):
+
+    def __init__(self, completed_callback=None):
+        self._listen()
+
+    def peon_task(self, user_function, id, args=[]):
+        pass
+
+    def _listen(self):
+
+        solutions = []
+
+        s = socket.socket()
+        s.bind((HOST, PORT))
+
+        for i in range(NUM_PEONS):
+            s.listen(0)
+            c, addr = s.accept()
+            print("Connection from", addr)
+
+            while 1:
+                c.send("Send the solution Peon")
+                peon_message = c.recv(BLOCK_SIZE)
+                print("Peon sends the solution ", peon_message)
+
+                if peon_message.strip() == "exit":
+                    print("closing connection", str(i))
+                    break
+                else:
+                    solutions.append(peon_message.strip())
+
+            c.close()
+
+        self._completed(solutions)
+
+    def _completed(self, solutions):
+        print(solutions)
+
+
+
+if __name__ == "__main__":
+    t = Task()
+
+# TODO: 
+# reboot peon
+# reboot all
+# retry subtask
+# non-blocking IO: select 
